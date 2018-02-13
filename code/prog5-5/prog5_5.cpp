c5069cb352b8865978dd891c81a7a0623cc011d3
Yiya Diao <dwzxdjt@126.com>
2018-02-14 01:53:46 +0800

    Update prog5_5.cpp

--- code/prog5-5/prog5_5.cpp
+++ code/prog5-5/prog5_5.cpp
@@ -5,9 +5,9 @@ void hanoi(int n, char src, char mid, char tar) {
 	if (n == 1)
 		cout << src << "-->" << tar << '\t';
 	else {
-		hanoi(n - 1, src, tar, mid); 
-		cout << src << "-->" << tar << '\t';
-		hanoi(n - 1, mid, src, tar);
+		hanoi(n - 1, src, tar, mid);//将n-1个盘子移到中间柱上
+	    cout << src << "-->" << tar << '\t';//将最后一个盘子移到目标柱上
+		hanoi(n - 1, mid, src, tar);//将n-1个盘子从中间柱移到目标柱上
 	}
 }
 int main() {
