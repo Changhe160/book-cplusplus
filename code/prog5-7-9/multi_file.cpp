d4a1215a93dae56b77bf64169733afadc95c3615
Yiya Diao <dwzxdjt@126.com>
2018-02-14 02:24:09 +0800

    Update multi_file.cpp

--- code/prog5-7-9/multi_file.cpp
+++ code/prog5-7-9/multi_file.cpp
@@ -1,10 +1,8 @@
-#include "myHeader.h"
+//multi_file.cpp
+#include "myHeader.h" // 包含用户自定义头文件
 #include <iostream>
-
 using namespace std;
-
 int main() {
 	g_sum = add(4, 5);
 	cout << g_sum << endl;
-
 }
