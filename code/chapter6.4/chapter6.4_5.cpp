0c5111ff4536a0a75c4d3779b25112d15ca95fb1
Changhe Li <changhe.lw@gmail.com>
2018-11-27 11:12:06 +0800

    code 6.4 updated

--- code/chapter6.4/chapter6.4_5.cpp
+++ code/chapter6.4/chapter6.4_5.cpp
@@ -1,45 +1,30 @@
-#include<string>
-#include<iostream>
-
-using namespace std;
-class PartTimeWorker {
-	string m_name;
-	double m_hours;
-	static double ms_payRate;
-public:
-	double salary() {		return m_hours*ms_payRate;	}
-	friend ostream& operator<<(ostream &o, PartTimeWorker& w);
-};
-double PartTimeWorker::ms_payRate = 7.53;
+#include "PartTime.h"
+#include <iostream>
 
 
-ostream& operator<<(ostream &o, PartTimeWorker& w) {
-	string PartTimeWorker::*p1 = &PartTimeWorker::m_name;
-	auto p2 = &PartTimeWorker::m_name;
-	
-
-	PartTimeWorker *w2 = &w;
-	cout << w.m_name << endl;
-	o << w.*p1 << endl;
-	o << w2->*p2 << endl;
-	
-	return o;
-}
 int main() {
-	
-	double (PartTimeWorker::*pf)();
-	pf = &PartTimeWorker::salary;
+	//6.4.2
+	{
+		cout << PartTimeWorker::rate() << endl; //通过类名访问静态成员
+		PartTimeWorker worker;
+		cout << worker.rate() << endl; //通过对象访问静态成员
+	}
+	//6.5.2
+	{
+		double (PartTimeWorker::*pf)();
+		pf = &PartTimeWorker::salary;
 
-	auto pf2= &PartTimeWorker::salary;
+		auto pf2 = &PartTimeWorker::salary;
 
-	using PTWS= double (PartTimeWorker::*)();
+		using PTWS = double (PartTimeWorker::*)();
 
-	PTWS pf3= &PartTimeWorker::salary;
+		PTWS pf3 = &PartTimeWorker::salary;
 
-	PartTimeWorker w;
-	cout << w.salary() << endl;
-	cout << (w.*pf)() << endl;
-	cout << (w.*pf2)() << endl;
+		PartTimeWorker w;
+		cout << w.salary() << endl;
+		cout << (w.*pf)() << endl;
+		cout << (w.*pf2)() << endl;
+	}
 
 }
 
